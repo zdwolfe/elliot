bccb83ac19b995e20e24ff0670e19211ab251f7c
Zach Wolfe <wolfe.zach@gmail.com>
2015-11-01 21:58:13 -0800

    yo angular elliot

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elliotApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the elliotApp
+ */
+angular.module('elliotApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elliotApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the elliotApp
+ */
+angular.module('elliotApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
